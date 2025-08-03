5bc6325706a233ba7b0cccbd8422e7e3a6db3828
sangeeta <sanjuother107@gamail.com>
2025-08-03 22:44:43 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const connectDB = require("./config/db");
- 
-// dotenv.config();
-// connectDB();
- 
-// const app = express();
- 
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
- 
-// // Routes
-// app.use("/api/appointments", require("./routes/appointmentRoutes"));
- 
-// const PORT = process.env.PORT || 5000;
- 
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
-
-
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -35,7 +11,7 @@ const app = express();
 // ✅ CORS Configuration
 app.use(
   cors({
-    origin: "https://doctor-appointment-frontend-fxx9.onrender.com", 
+    origin: "https://doctor-appointment-lake-one.vercel.app ", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
